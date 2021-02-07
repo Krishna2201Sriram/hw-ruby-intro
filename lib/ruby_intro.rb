9b55ecf64b535ae933693006b1fef43e8f19acb6
EC2 Default User <ec2-user@ip-172-31-3-190.ca-central-1.compute.internal>
2021-02-07 07:42:55 +0000

    part2 completed

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -28,14 +28,29 @@ end
 
 def hello(name)
   # YOUR CODE HERE
+  "Hello, #{name}"
 end
 
 def starts_with_consonant? s
   # YOUR CODE HERE
+  return false if s.empty?
+  if s=~/\A[^aeiou\W\d_]/i
+	  return true
+  else
+	  return false
+  end
 end
 
 def binary_multiple_of_4? s
   # YOUR CODE HERE
+  return false if s.empty?
+  if s.to_i(2) == 0
+    return false
+  elsif s.to_i(2) % 4 == 0
+    return true
+  else
+    return false
+  end
 end
 
 # Part 3
